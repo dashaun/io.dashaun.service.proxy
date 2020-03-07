25497e9cdf58891c828a6c2badd879580f467424
DaShaun <dashaun@dashaun.com>
2020-03-07 17:17:38 -0600

    Enough to get some cool metrics

--- src/main/java/io/dashaun/service/proxy/Application.java
+++ src/main/java/io/dashaun/service/proxy/Application.java
@@ -2,10 +2,23 @@ package io.dashaun.service.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
 	}
 }
